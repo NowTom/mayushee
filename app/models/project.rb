46385e405e10b6cb5d84d328d621edf1a911bb75
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2013-05-26 15:21:23 +0900

    Upgrade to rails 4

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,5 @@ class Project < ActiveRecord::Base
   has_many :users, through: :duties
   has_many :roles, through: :duties
   has_many :sprints
-  has_many :stories, order: 'importance DESC'
+  has_many :stories
 end
